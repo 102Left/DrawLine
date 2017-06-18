c59faa12e2e0b8d8e18e27cc83deadb9900c176f
川上竜乃進 <wccrs287@yahoo.co.jp>
2017-06-18 22:33:29 +0900

    初期状態

---
+++ DrawLine/Line.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DrawLine
+{
+    class Line
+    {
+        public List<Point> Points { get; set; }
+
+        public Line()
+        {
+            this.Points = new List<Point>();
+        }
+        public Line(List<Point> points)
+        {
+            this.Points = new List<Point>(points);
+        }
+        public Line(Line source)
+        {
+            this.Points = new List<Point>(source.Points);
+        }
+    }
+}
